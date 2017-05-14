a43a99cd34000749226d5d331507e768c056d312
Victor Mora <victor.mora.benambourg@gmail.com>
2017-05-14 15:10:04 -0600

    Barra de aplicacion personalizada implementada

--- app/src/main/java/com/mora/victor/petagram/MainActivity.java
+++ app/src/main/java/com/mora/victor/petagram/MainActivity.java
@@ -2,12 +2,18 @@ package com.mora.victor.petagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar myActionbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myActionbar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(myActionbar);
     }
 }
